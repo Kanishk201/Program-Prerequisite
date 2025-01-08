eee20708247916258fe8c56177e92179dc8b1578
Kanishk201 <kanisyadav88780@gmail.com>
2025-01-08 12:10:14 +0530

    Calculate Simple Interest

---
+++ SimpleInterest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Enter the principal amount: ");
+        double principal = scanner.nextDouble();
+
+        System.out.println("Enter the rate of interest (in %): ");
+        double rate = scanner.nextDouble();
+
+        System.out.println("Enter the time (in years): ");
+        double time = scanner.nextDouble();
+
+        
+        double simpleInterest = (principal * rate * time) / 100;
+
+        
+        System.out.println("The simple interest for a principal of " + principal +
+                           ", rate of " + rate + "%, and time of " + time +
+                           " years is: " + simpleInterest);
+
+        
+    }
+}
