f542c3abaa2f2a3ce67590f10ad1bf42b71bd885
Kanishk201 <kanisyadav88780@gmail.com>
2025-01-08 11:53:46 +0530

    Calculate power

---
+++ PowerCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PowerCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.println("Enter the base: ");
+        double base = scanner.nextDouble();
+
+        System.out.println("Enter the exponent: ");
+        int exponent = scanner.nextInt();
+
+        
+        double result = Math.pow(base, exponent);
+
+        
+        System.out.println(base + " raised to the power of " + exponent + " is: " + result);
+
+        
+    }
+}
